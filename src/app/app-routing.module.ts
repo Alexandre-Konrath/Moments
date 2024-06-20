382addee9fd864de6fea71d5ee45a1a903b10be0
Alexandre-Konrath <delimakonrath@gmail.com>
2024-06-20 08:28:25 -0300

    arquivo de rotas -  onde todas as rotas da pagina são criadas que levam para seus arquivos de componentes que foram importados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NewMomentsComponent } from './pages/new-moments/new-moments.component';
+import { MomentComponent } from './pages/moment/moment.component';
+import { EditComentComponent } from './pages/edit-coment/edit-coment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'moments/new', component: NewMomentsComponent },
+  { path: 'moments/edit/:id', component: EditComentComponent },
+  { path: 'moments/:id', component: MomentComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
