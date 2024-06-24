0df96e43b13624868d4fc5ffac1b04b9eeaa108a
Alexandre-Konrath <delimakonrath@gmail.com>
2024-06-24 08:32:36 -0300

    arquivo de importação de componentes e de bibliotecas do Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NewMomentsComponent } from './pages/new-moments/new-moments.component';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MomentComponent } from './pages/moment/moment.component';
+import { EditComentComponent } from './pages/edit-coment/edit-coment.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    NewMomentsComponent,
+    MomentFormComponent,
+    MessagesComponent,
+    MomentComponent,
+    EditComentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
